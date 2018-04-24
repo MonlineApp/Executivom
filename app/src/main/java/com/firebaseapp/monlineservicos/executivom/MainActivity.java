1b69bbe8346549bd3c0cceb261336d77159ab68d
Celso Henrique <36873697+MonlineApp@users.noreply.github.com>
2018-04-24 18:22:42 -0300

    Add files via upload

---
+++ app/src/main/java/com/firebaseapp/monlineservicos/executivom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.firebaseapp.mservice.executivom;
+package com.firebaseapp.mservice.executivom
+
+/**
+ *
+ */
+@SuppressWarnings("ALL")
+public class com.firebaseapp.mservice.executivom MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
